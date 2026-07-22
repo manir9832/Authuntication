459e78d0980836830c5688ae0347fd0218d078d6
Manir Hassan <manirhassan9832@gmail.com>
2026-07-22 20:48:41 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -10,12 +10,16 @@ const PORT=process.env.PORT||2000;
 app.get('/',(req,res)=>{
    res.send('server running');
 })
-app.use(cors(
-    {
-    origin: process.env.CLINT_URL ,
-    credentials: true,
-  }
-))
+app.use(
+  cors({
+    origin: [
+      "https://authuntication.vercel.app", // 👈 মূল প্রডাকশন URL
+      process.env.CLINT_URL, // 👈 প্রিভিউ URL
+      "http://localhost:5173", // Localhost টেস্টিংয়ের জন্য
+    ],
+    credentials: true, // Cookies পাঠানোর জন্য অত্যন্ত জরুরি
+  })
+);
 app.use(express.json());
 app.use(cookieParser())
 
