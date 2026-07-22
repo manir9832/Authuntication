0ab06d3094b2a71fd429687a72e90f046daacbb7
Manir Hassan <manirhassan9832@gmail.com>
2026-07-22 09:38:21 +0530

    new

--- backend/utils/generateTokenAndSetCookie.js
+++ backend/utils/generateTokenAndSetCookie.js
@@ -6,7 +6,7 @@ const generateTokenAndSetCookie =(res,userId)=>{
     res.cookie("token",token,{
         httpOnly:true,
         secure:process.env.NODE_ENV==="production",
-        sameSite:"strict",
+        sameSite: isProduction ? "none" : "lax",
         maxAge:7*24*60*60*1000
     });
 }
