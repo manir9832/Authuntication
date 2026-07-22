b7bdd62fec3f7aa5f16dba32e7f8e166167e958b
Manir Hassan <manirhassan9832@gmail.com>
2026-07-22 20:23:03 +0530

    commit

--- backend/utils/generateTokenAndSetCookie.js
+++ backend/utils/generateTokenAndSetCookie.js
@@ -1,15 +1,19 @@
-const jwt=require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 
-const generateTokenAndSetCookie =(res,userId)=>{
-    const token=jwt.sign({userId},process.env.JWT_SECRET,{expiresIn:"2h"});
+const generateTokenAndSetCookie = (res, userId) => {
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "2h" });
 
-    res.cookie("token",token,{
-        httpOnly:true,
-        secure:process.env.NODE_ENV==="production",
-        sameSite: isProduction ? "none" : "lax",
-        maxAge:7*24*60*60*1000
+    // 💡 এই লাইনটি মিসিং ছিল:
+    // const isProduction = process.env.NODE_ENV === "production";
+
+    res.cookie("token", token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+        maxAge: 7 * 24 * 60 * 60 * 1000
     });
-}
 
+    return token;
+};
 
-module.exports=generateTokenAndSetCookie;
+module.exports = generateTokenAndSetCookie;
